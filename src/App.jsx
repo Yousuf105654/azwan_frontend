36c91531dbeebfe868160136b391e82d2937ff64
Yousuf <108928113+Yousuf105654@users.noreply.github.com>
2025-03-01 19:30:39 +0600

    Update App.jsx
    Updated response link

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ const Chatbot = () => {
     setInput("");
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/chat/", {
+      const response = await fetch("https://azwan-chatbot-backend.onrender.com/chat/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: input }),
